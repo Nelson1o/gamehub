68c083e1581a7f418564c080fe9bfaa657622154
Anton <anton.krivozubov@gmail.com>
2026-06-18 19:52:49 +0300

    feat: add sorting with custom dropdown and URL sync

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -2,3 +2,4 @@ export { useInfiniteScroll } from "./use-infinite-scroll";
 export { useIsGamePage } from "./use-is-game-page";
 export { useLightBox } from "./use-lightbox";
 export { usePageTitle } from "./use-page-title";
+export { useSortParams } from "./use-sort-params";
